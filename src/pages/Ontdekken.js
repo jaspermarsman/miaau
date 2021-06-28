5610b90adcc4e1c68b1cb1fcc8de02a01479051d
jaspermarsman <jaspermarsman@gmail.com>
2021-06-28 11:15:25 +0200

    added some styling to dropdown

--- src/pages/Ontdekken.js
+++ src/pages/Ontdekken.js
@@ -1,18 +1,10 @@
 import React, {useState, useContext} from "react";
-import "./Ontdekken.css"
 import Multiselect from 'multiselect-react-dropdown';
 import { selectedOptionsContext} from "../context/SelectedOptionsProvider";
 
 export default function Ontdekken() {
     // const [selected, setSelected] = useState(countries);
     const { selectedCountries, setSelectedCountries } = useContext(selectedOptionsContext);
-
-    // useEffect(() => {
-    //
-    //         setSelected(selected);
-    //     console.log("selected: ", selected)
-    //
-    // }, [selected])
 
     const onSelect = (selectedCountries) => {
         console.log("onSelect" ,selectedCountries)
@@ -23,17 +15,43 @@ export default function Ontdekken() {
     }
 
     return(
-        <div className="ontdekken">
+        <div className="login">
             <Multiselect
-                options={selectedCountries}
-                displayValue="name"
-                onSelect={onSelect}
-                showCheckbox={true}
-                onRemove={onRemove}
+                options = {selectedCountries}
+                displayValue = "name"
+                onSelect = {onSelect}
+                showCheckbox = {true}
+                onRemove = {onRemove}
+                style={{
+                    chips: {
+                        background: 'red'
+                    },
+                    multiselectContainer: {
+                        color: '#E9C46A',
+                        width: "50%",
+                        "border-color": "black",
+                        "border-radius": "5px"
+                    },
+                    searchBox: {
+                        border: 'none',
+                        'border-radius': '5px',
+                        background: "white"
+                    },
+                    optionContainer: {
+                        background: "black"
+                    },
+                    option: {
+                        display: "flex",
+                        color: "#E9C46A"
+                    },
+                    "option:hover": {
+                        color: "red"
+                }
+                }}
 
             />
             {/*{console.log("selected: ", selected)}*/}
-
+<p>shdjfghjksdgh</p>
         </div>
     );
 };
